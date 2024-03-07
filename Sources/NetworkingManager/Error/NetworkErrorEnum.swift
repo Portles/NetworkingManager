5e891378d1970f23a8389d6ba4847d07ac4171fd
Nizamet Özkan <nizamet005@gmail.com>
2024-03-07 20:55:58 +0300

    Updates

---
+++ Sources/NetworkingManager/Error/NetworkErrorEnum.swift
@@ -0,0 +1,11 @@
+//
+//  NetworkErrorEnum.swift
+//
+//
+//  Created by Nizamet Özkan on 7.03.2024.
+//
+
+public enum NetworkError: Error {
+    case badRequest
+    case decodingError
+}
--- Sources/NetworkingManager/NetworkingManager.swift
+++ Sources/NetworkingManager/NetworkingManager.swift
@@ -1,2 +1,36 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public class Webservice {
+    public init() { }
+    
+    public func fetch<T: Codable>(url: URL, completion: @escaping (Result<T, Error>) -> Void) {
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+
+            if let httpResponse = response as? HTTPURLResponse, !(200...299).contains(httpResponse.statusCode) {
+                completion(.failure(NSError(domain: "", code: httpResponse.statusCode, userInfo: nil)))
+                return
+            }
+
+            guard let data = data else {
+                completion(.failure(NSError(domain: "", code: -1003, userInfo: nil)))
+                return
+            }
+
+            do {
+                let decodedData = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(decodedData))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+}
+    
